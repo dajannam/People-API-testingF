2278141f48fb60fed0b6528b236cc22a6e75129b
dajannam <dajana_miteva95@yahoo.com>
2021-09-16 23:22:43 +0200

    http client

--- src/main/java/code/academy/client/PeopleApiClient.java
+++ src/main/java/code/academy/client/PeopleApiClient.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
@@ -22,7 +23,7 @@ import javax.net.ssl.SSLContext;
 public class PeopleApiClient {
 
 
-    public HttpResponse getWelcomeRequest() throws Exception {
+    public HttpResponse httpGet(String url) throws Exception {
 
         Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 
@@ -31,11 +32,10 @@ public class PeopleApiClient {
                 .loadTrustMaterial(new TrustSelfSignedStrategy())
                 .build();
 
-        HttpGet request = new HttpGet("https://people-api1.herokuapp.com");
+        HttpGet request = new HttpGet(url);
         request.setHeader(contentType);
 
         HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-
         HttpResponse response = httpClient.execute(request);
 
         HttpEntity entity = response.getEntity();
@@ -47,9 +47,7 @@ public class PeopleApiClient {
         return response;
 
     }
-
-    public HttpResponse getPeople() throws Exception {
-
+    public HttpResponse httpDelete(String url) throws Exception {
         Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 
         SSLContext sslContext = SSLContextBuilder
@@ -57,52 +55,20 @@ public class PeopleApiClient {
                 .loadTrustMaterial(new TrustSelfSignedStrategy())
                 .build();
 
-        HttpGet request = new HttpGet("https://people-api1.herokuapp.com/api/people");
+        HttpDelete request = new HttpDelete(url);
         request.setHeader(contentType);
 
         HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-
         HttpResponse response = httpClient.execute(request);
-
         HttpEntity entity = response.getEntity();
         String body = EntityUtils.toString(response.getEntity());
-
         HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
         response.setEntity(newEntity);
-
-        return response;
-
-
-    }
-
-    public HttpResponse getOnePerson() throws Exception {
-
-        Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-
-        SSLContext sslContext = SSLContextBuilder
-                .create()
-                .loadTrustMaterial(new TrustSelfSignedStrategy())
-                .build();
-
-        HttpGet request = new HttpGet("https://people-api1.herokuapp.com/api/person/612ba20357744c30dc7e6fe7");
-        request.setHeader(contentType);
-
-        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-
-        HttpResponse response = httpClient.execute(request);
-
-        HttpEntity entity = response.getEntity();
-        String body = EntityUtils.toString(response.getEntity());
-
-        HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
-        response.setEntity(newEntity);
-
         return response;
 
     }
 
-    public HttpResponse postNewPerson() throws Exception {
-
+    public HttpResponse httpPost(String url, JSONObject payload) throws Exception {
         Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 
         SSLContext sslContext = SSLContextBuilder
@@ -110,33 +76,21 @@ public class PeopleApiClient {
                 .loadTrustMaterial(new TrustSelfSignedStrategy())
                 .build();
 
-        HttpPost request = new HttpPost("https://people-api1.herokuapp.com/api/person");
-
-        JSONObject payloadAsObject = new JSONObject();
-        payloadAsObject.put("name", "Pero");
-        payloadAsObject.put("surname", "Blazevski");
-        payloadAsObject.put("age", 56);
-        payloadAsObject.put("isEmployed", "true");
-        payloadAsObject.put("location", "Skopje");
-
+        HttpPost request = new HttpPost(url);
         request.setHeader(contentType);
-        request.setEntity(new StringEntity(payloadAsObject.toString()));
+        request.setEntity(new StringEntity(payload.toString()));
 
         HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
 
         HttpResponse response = httpClient.execute(request);
-
         HttpEntity entity = response.getEntity();
         String body = EntityUtils.toString(response.getEntity());
-
         HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
         response.setEntity(newEntity);
-
         return response;
 
     }
-
-    public HttpResponse putNewLocation() throws Exception {
+    public HttpResponse httpPut(String url, JSONObject payload) throws Exception {
 
         Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 
@@ -145,25 +99,18 @@ public class PeopleApiClient {
                 .loadTrustMaterial(new TrustSelfSignedStrategy())
                 .build();
 
-        HttpPut request = new HttpPut("https://people-api1.herokuapp.com/api/person/613de5c9dd85560004b265ae");
-
-        JSONObject payloadAsObject = new JSONObject();
-        payloadAsObject.put("location", "San Francisko");
-
-
+        HttpPut request = new HttpPut(url);
         request.setHeader(contentType);
-        request.setEntity(new StringEntity(payloadAsObject.toString()));
+        request.setEntity(new StringEntity(payload.toString()));
 
         HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
 
         HttpResponse response = httpClient.execute(request);
-
         HttpEntity entity = response.getEntity();
         String body = EntityUtils.toString(response.getEntity());
-
         HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
         response.setEntity(newEntity);
-
         return response;
+
     }
 }
--- src/test/java/code/academy/peopleapi/InitialTestFile.java
+++ src/test/java/code/academy/peopleapi/InitialTestFile.java
@@ -4,6 +4,8 @@ package code.academy.peopleapi;
 import code.academy.client.PeopleApiClient;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -12,22 +14,95 @@ public class InitialTestFile {
 
     PeopleApiClient peopleApiClient = new PeopleApiClient();
     HttpResponse response;
-    HttpResponse getPeople;
-    HttpResponse getOnePerson;
+
 
     @Test
-    public void initialTest() throws Exception {
 
-        response = peopleApiClient.getWelcomeRequest();
-        getPeople = peopleApiClient.getPeople();
-        getOnePerson = peopleApiClient.getOnePerson();
-        response = peopleApiClient.postNewPerson();
-        response = peopleApiClient.putNewLocation();
-
+    public void welcomeMessagePeopleApiTest() throws Exception {
+        String expectedMessage = "Welcome to People API";
+        response = peopleApiClient.httpGet("https://people-api1.herokuapp.com/");
 
         String body = EntityUtils.toString(response.getEntity());
-        String bodyAllPeople = EntityUtils.toString(getPeople.getEntity());
-        String bodyOfOnePerson = EntityUtils.toString(getOnePerson.getEntity());
+        JSONObject bodyAsObject = new JSONObject(body);
+
+        String messageAsString = bodyAsObject.get("message").toString();
+
+        Assert.assertEquals(messageAsString, expectedMessage);
+    }
+
+    @Test
+
+    public void getSinglePersonTest() throws Exception {
+        String expectedMessage = "Person succesfully fetched";
+
+        response = peopleApiClient.httpGet("https://people-api1.herokuapp.com/api/person/612ba20357744c30dc7e6fe7");
+
+        String body = EntityUtils.toString(response.getEntity());
+        JSONObject bodyAsObject = new JSONObject(body);
+
+        String personDataAsString = bodyAsObject.get("person").toString();
+        JSONObject personData = new JSONObject(personDataAsString);
+
+        String name = personData.get("name").toString();
+
+        Assert.assertEquals(name, "Stefan");
+
 
     }
+    @Test
+    public void getAllPeopleTest() throws Exception {
+        String expectedMessage = "List of people successfully fetched";
+
+        response = peopleApiClient.httpGet("https://people-api1.herokuapp.com/api/people");
+        String body = EntityUtils.toString(response.getEntity());
+
+        JSONObject bodyAsObject = new JSONObject(body);
+
+        String messageAsString = bodyAsObject.get("message").toString();
+
+        Assert.assertEquals(messageAsString, expectedMessage);
+
     }
+    @Test
+    public void postPersonTest() throws Exception {
+        String expectedMessage ="Person succesfully inserted";
+
+        JSONObject payloadAsObject = new JSONObject();
+        payloadAsObject.put("name", "Pero");
+        payloadAsObject.put("surname", "Blazevski");
+        payloadAsObject.put("age", 56);
+        payloadAsObject.put("isEmployed", true);
+        payloadAsObject.put("location", "Skopje");
+
+        response = peopleApiClient.httpPost("https://people-api1.herokuapp.com/api/person", payloadAsObject);
+        String body = EntityUtils.toString(response.getEntity());
+
+        JSONObject bodyAsObject = new JSONObject(body);
+
+        String messageAsString = bodyAsObject.get("message").toString();
+
+        Assert.assertEquals(messageAsString, expectedMessage);
+
+    }
+
+    @Test
+    public void updatePersonLocationTest() throws Exception {
+
+        String expectedMessage ="Person's location succesfully updated !";
+
+        JSONObject payloadAsObject = new JSONObject();
+        payloadAsObject.put("location", "London");
+
+        response = peopleApiClient.httpPut("https://people-api1.herokuapp.com/api/person/613de5c9dd85560004b265ae",
+                payloadAsObject);
+
+        String body = EntityUtils.toString(response.getEntity());
+        JSONObject bodyAsObject = new JSONObject(body);
+
+        String messageAsString = bodyAsObject.get("message").toString();
+
+        Assert.assertEquals(messageAsString, expectedMessage);
+
+    }
+}
+
